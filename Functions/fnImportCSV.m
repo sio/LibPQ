53a31d3516ad28e63f842cab92fb5496090828da
Vitaly Potyarkin <sio.wtf@gmail.com>
2017-11-28 15:20:59 +0300

    Add docstrings and comments

--- Functions/fnImportCSV.m
+++ Functions/fnImportCSV.m
@@ -1,17 +1,20 @@
-(tsv_path, first_column_header, optional encoding, optional delimiter) => let
-    /*
-    fnImportCSV
-
+/**
+fnImportCSV:
     Load arbitrary CSV file with unknown number of columns (up to 1000)
     and unknown number of extra lines before the column names (first column name
     has to be known).
 
     Note: Columns which names start with underscore ("_") will be lost, if
           there are any in the original CSV.
-    */
+**/
 
+(tsv_path, first_column_header, optional encoding, optional delimiter) => let
+
+    /* Default parameter values */
     Delimiter = if delimiter is null then "#(tab)" else delimiter,
     Encoding = if encoding is null then 65001 else encoding,
+
+    /* Load CSV */
     Source = Csv.Document(
                 File.Contents(tsv_path),
                 [
--- Functions/fnMakeComparer.m
+++ Functions/fnMakeComparer.m
@@ -1,17 +1,24 @@
+/**
+fnMakeComparer:
+    Factory for creating comparison functions to be used later
+    for filling ErrorCheck columns
+**/
+
 (optional field1     as nullable text,
  optional field2     as nullable text,
  optional operator   as nullable text,
  optional expression as nullable text) => let
-    /*
-    Factory for creating comparison functions to be used later
-    for filling ErrorCheck columns
-    */
 
+    /* Default parameter values */
     Operator = if operator is null then "=" else operator,
+
+    /* Helper function that enables passing "null" as text */
     GetFieldOrNull = (record as record, field as text) =>
         if   field = "null"
         then null
         else Record.Field(record, field),
+
+    /* New function generator */
     NewFunction = (record as record) => let
         Result =
             if      expression <> null
